08168cc7290f0e487b346488ac59662f2f55bb8f
Macarena Acosta <78175961+indianazaraza@users.noreply.github.com>
2022-01-02 22:03:57 -0300

    adding comments

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import socket
 
+#instance the module
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+#bind to port from localhost
 my_socket.bind(('localhost', 8000))
+#listen five request
 my_socket.listen(5)
 
 while 1:
+	#accept request from client
 	connection, address = my_socket.accept()
 
 	print(address)
-
+	
+	#receive message from client
 	request = connection.recv(1024).decode()
 	print(request)
-
+	
+	#send message to client
 	connection.send("servidor")
 	
 	connection.close()
