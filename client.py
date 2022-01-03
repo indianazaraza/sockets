2615f6d0aff82c1a9d0d29aed67420a3899f388d
Macarena Acosta <78175961+indianazaraza@users.noreply.github.com>
2022-01-02 21:59:43 -0300

    add comments

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
 
+#instance the module
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 my_socket.connect(("localhost", 8000))
 
+#send message to server
 my_socket.send("cliente")
+
+#receive message from server
 response = my_socket.recv(1024).decode()
 print(response)
 
+#close connection
 my_socket.close()
